4ded2bb392ffaded280e634379281a1ad82be374
Thegold777 <165582387+Thegold777@users.noreply.github.com>
2024-06-18 22:05:02 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
 let images = [{
-       url:'imgs/rostov.png'
+    url: 'imgs/rostov.png'
 }, {
-        url:'imgs/sochi.png'
-    }, {
-    url:'imgs/rostov 2.png'
+    url: 'imgs/sochi.png'
+}, {
+    url: 'imgs/rostov 2.png'
 }
 ]
 let info = [{
-    city:'Rostov-on-Don\n' +
+    city: 'Rostov-on-Don\n' +
         'LCD admiral',
-    area:'81 m2',
-    time:'3.5 months'
-},{
-    city:'Sochi\n' +
+    area: '81 m2',
+    time: '3.5 months'
+}, {
+    city: 'Sochi\n' +
         'Thieves',
-    area:'105 m2',
-    time:'4 months'
-},{
-    city:'Rostov-on-Don\n' +
+    area: '105 m2',
+    time: '4 months'
+}, {
+    city: 'Rostov-on-Don\n' +
         'Patriotic',
-    area:'93 m2',
-    time:'3 months'
+    area: '93 m2',
+    time: '3 months'
 }
 ]
-function ChangeInfo1(){
-    document.querySelector('.main__text_city').innerText= info[0].city
-    document.querySelector('.main__text_area').innerText= info[0].area
-    document.querySelector('.main__text_time').innerText= info[0].time
+function ChangeInfo1() {
+    document.querySelector('.main__text_city').innerText = info[0].city
+    document.querySelector('.main__text_area').innerText = info[0].area
+    document.querySelector('.main__text_time').innerText = info[0].time
 }
-function ChangeInfo2(){
-    document.querySelector('.main__text_city').innerText= info[1].city
-    document.querySelector('.main__text_area').innerText= info[1].area
-    document.querySelector('.main__text_time').innerText= info[1].time
+function ChangeInfo2() {
+    document.querySelector('.main__text_city').innerText = info[1].city
+    document.querySelector('.main__text_area').innerText = info[1].area
+    document.querySelector('.main__text_time').innerText = info[1].time
 }
-function ChangeInfo3(){
-    document.querySelector('.main__text_city').innerText= info[2].city
-    document.querySelector('.main__text_area').innerText= info[2].area
-    document.querySelector('.main__text_time').innerText= info[2].time
+function ChangeInfo3() {
+    document.querySelector('.main__text_city').innerText = info[2].city
+    document.querySelector('.main__text_area').innerText = info[2].area
+    document.querySelector('.main__text_time').innerText = info[2].time
 }
- function ChangeInfo() {
+function ChangeInfo() {
     if (i === 0) {
         ChangeInfo1()
     }
-    else  if (i === 1) {
+    else if (i === 1) {
         ChangeInfo2()
     }
-    else  if (i === 2) {
+    else if (i === 2) {
         ChangeInfo3()
     }
 }
@@ -67,8 +67,8 @@ async function arrowRight() {
         document.querySelector('.main__img').src = images[i].url;
     }
 }
-rightArrow.addEventListener('click',arrowRight)
-rightArrow.addEventListener('click',ChangeInfo)
+rightArrow.addEventListener('click', arrowRight)
+rightArrow.addEventListener('click', ChangeInfo)
 
 async function arrowLeft() {
     if (i === 0) {
@@ -77,7 +77,7 @@ async function arrowLeft() {
         textActive()
         document.querySelector('.main__img').src = images[i].url;
     }
-    else   {
+    else {
         i = i - 1;
         ChangeInfo()
         textActive()
@@ -85,7 +85,7 @@ async function arrowLeft() {
     }
 
 }
-leftArrow.addEventListener('click',arrowLeft)
+leftArrow.addEventListener('click', arrowLeft)
 let c = 0;
 const text1 = document.querySelector('.main__names_text_1');
 const text2 = document.querySelector('.main__names_text_2');
@@ -103,19 +103,19 @@ function textActive() {
         dotClick3()
     }
 }
-function textActive1(){
-     if ( document.querySelector('.main__names_text_1').style.color !== '#E3B873') {
-         i = 0
-         ChangeInfo()
-         document.querySelector('.main__img').src = images[i].url;
-         document.querySelector('.main__names_text_1').style.color = '#E3B873'
-         document.querySelector('.main__names_text_1').style.textDecoration = 'underline'
-         document.querySelector('.main__names_text_2').style.color = '#FFFFFF4D'
-         document.querySelector('.main__names_text_2').style.textDecoration = 'none'
-         document.querySelector('.main__names_text_3').style.color = '#FFFFFF4D'
-         document.querySelector('.main__names_text_3').style.textDecoration = 'none'
-         dotClick1()
-     }
+function textActive1() {
+    if (document.querySelector('.main__names_text_1').style.color !== '#E3B873') {
+        i = 0
+        ChangeInfo()
+        document.querySelector('.main__img').src = images[i].url;
+        document.querySelector('.main__names_text_1').style.color = '#E3B873'
+        document.querySelector('.main__names_text_1').style.textDecoration = 'underline'
+        document.querySelector('.main__names_text_2').style.color = '#FFFFFF4D'
+        document.querySelector('.main__names_text_2').style.textDecoration = 'none'
+        document.querySelector('.main__names_text_3').style.color = '#FFFFFF4D'
+        document.querySelector('.main__names_text_3').style.textDecoration = 'none'
+        dotClick1()
+    }
 }
 function textActive2() {
     if (document.querySelector('.main__names_text_2').style.color !== '#E3B873') {
@@ -131,8 +131,8 @@ function textActive2() {
         dotClick2()
     }
 }
-function textActive3(){
-    if ( document.querySelector('.main__names_text_3').style.color !== '#E3B873') {
+function textActive3() {
+    if (document.querySelector('.main__names_text_3').style.color !== '#E3B873') {
         i = 2
         ChangeInfo()
         document.querySelector('.main__img').src = images[i].url;
@@ -145,36 +145,36 @@ function textActive3(){
         dotClick3()
     }
 }
-text1.addEventListener('click',textActive1)
-text2.addEventListener('click',textActive2)
-text3.addEventListener('click',textActive3)
+text1.addEventListener('click', textActive1)
+text2.addEventListener('click', textActive2)
+text3.addEventListener('click', textActive3)
 
 const dot1 = document.querySelector('.main__slider_dots_1')
 const dot2 = document.querySelector('.main__slider_dots_2')
 const dot3 = document.querySelector('.main__slider_dots_3')
 function dotClick1() {
     i = 0
-    dot1.style.background ='white'
-    dot2.style.background ='#FFFFFF4D'
-    dot3.style.background ='#FFFFFF4D'
+    dot1.style.background = 'white'
+    dot2.style.background = '#FFFFFF4D'
+    dot3.style.background = '#FFFFFF4D'
     textActive1()
 }
 function dotClick2() {
     i = 1
-    dot2.style.background ='white'
-    dot1.style.background ='#FFFFFF4D'
-    dot3.style.background ='#FFFFFF4D'
+    dot2.style.background = 'white'
+    dot1.style.background = '#FFFFFF4D'
+    dot3.style.background = '#FFFFFF4D'
     textActive2()
 }
 function dotClick3() {
     i = 2
-    dot3.style.background ='white'
-    dot1.style.background ='#FFFFFF4D'
-    dot2.style.background ='#FFFFFF4D'
+    dot3.style.background = 'white'
+    dot1.style.background = '#FFFFFF4D'
+    dot2.style.background = '#FFFFFF4D'
     textActive3()
     dot1.add('hover-style')
     dot2.add('hover-style')
 }
-dot1.addEventListener('click',dotClick1)
-dot2.addEventListener('click',dotClick2)
-dot3.addEventListener('click',dotClick3)
+dot1.addEventListener('click', dotClick1)
+dot2.addEventListener('click', dotClick2)
+dot3.addEventListener('click', dotClick3)
